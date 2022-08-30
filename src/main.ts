a7d4033a8edc15827bd7694b6aa15a71371ac9fc
Carlos DaLomba <carlos@lookingglassfactory.com>
2022-08-30 15:01:37 -0400

    Fixed up syntax to include data types

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,18 @@
-const { app, BrowserWindow } = require("electron");
+//NOTE: Use this if you want to use the fully qualified names, such as Electron.BrowserWindow, Electron.app, etc.:
+// import Electron from "electron";
 
-const createWindow = () => {
-    const win = new BrowserWindow({
+//NOTE: Use this if you want to use the shorthand names without needing to write
+//Electron.BrowserWindow and Electron.app for Browserwindow and app, rsepectively:
+import { app, BrowserWindow } from "electron";
+
+function createWindow() : BrowserWindow {
+    const window : BrowserWindow = new BrowserWindow({
         width: 800,
         height: 600
     });
 
-    win.loadFile("src/index.html");
+    window.loadFile("src/index.html");
+    return window;
 };
 
-app.whenReady().then(() => {
-    createWindow();
-});
+app.whenReady().then(createWindow);
