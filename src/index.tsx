1cdcf8a32c037d7fad9995067ecaddc27507018c
Carlos DaLomba <carlos@lookingglassfactory.com>
2022-09-09 19:19:03 -0400

    Fixed ReactJS builds to use TS & added dedicated electron folder

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './react/App';
 
 ReactDOM.render(<App/>, document.getElementById('app'));
+
+/**
+ * This does something!
+ * @returns 48
+ */
+function example() : number {
+    return 48;
+}
